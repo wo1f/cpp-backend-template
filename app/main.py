b20810661aec7459187f33726538071481dfe57c
Oleg Skinderev <>
2022-06-10 20:33:40 +0300

    small fix

--- app/main.py
+++ app/main.py
@@ -15,15 +15,13 @@ def pong():
 
 
 @app.route('/hello/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
+def hello(username):
     return f'Hello {escape(username)}!'
 
 
-# @app.route('/age/<int:post_id>')
-# def show_post(post_id):
-#     # show the post with the given id, the id is an integer
-#     return f'Post {post_id}'
+# @app.route('/goodbye/<username>')
+# def goodbye(username):
+#     return f'Goodbye {escape(username)}!'
 
 
 if __name__ == '__main__':
